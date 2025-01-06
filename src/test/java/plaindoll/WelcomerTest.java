501a70983fb09d364ed68e282331b0ede5a0b719
ne0kk <neok.kem@gmail.com>
2025-01-06 16:42:07 +0700

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -4,7 +4,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+ 
 public class WelcomerTest {
 	
 	private Welcomer welcomer = new Welcomer();
